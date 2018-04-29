dad32763b4aefc54ef1968944ccb7163c266af8d
sol wu <apiswswsw@gmail.com>
2018-04-29 09:48:55 -0700

    serve the dynamic aggregate result

--- server.js
+++ server.js
@@ -1,12 +1,52 @@
 var express = require('express')
 var app = express()
 
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+// Connection URL
+var dbs = "inactivemonitor";
+var url = 'mongodb://localhost:27017/';
+
+let dbo;
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server");
+
+  dbo = db.db(dbs);
+
+});
+
 // app.get('/', function (req, res) {
 //   res.send('Hello World!')
 // })
+
+app.get('/groupbytime', function (req, res) {
+
+  aggregate(dbo, function(docs) {
+    res.send(JSON.stringify(docs));
+  });
+})
 
 app.use(express.static('public'));
 
 app.listen(8000, function () {
   console.log('listening on port 8000!')
 })
+
+
+//========================
+
+
+var aggregate = function(dbo, callback) {
+  var collection = dbo.collection('last_tx_time');
+
+  collection.aggregate([{"$group" : {_id:"$last_tx_time", count:{$sum:1}}}])
+    .toArray(function(err, docs) {
+      assert.equal(err, null);
+      console.log("Found the following records");
+      console.log(docs)
+      callback(docs);
+  });
+}
